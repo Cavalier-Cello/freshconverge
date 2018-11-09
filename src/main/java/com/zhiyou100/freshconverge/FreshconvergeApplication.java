3283cd40c7132b42d211eae7e947ca15c2283d90
Administrator <695597142@qq.com>
2018-11-09 15:06:08 +0800

    Initial commit

---
+++ src/main/java/com/zhiyou100/freshconverge/FreshconvergeApplication.java
@@ -0,0 +1,12 @@
+package com.zhiyou100.freshconverge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshconvergeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshconvergeApplication.class, args);
+    }
+}
